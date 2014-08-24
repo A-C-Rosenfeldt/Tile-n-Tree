7853cf409dce0d0a986d19c00d83f7a1d019bf7f
Arne Christian  Rosenfeldt <Arne.Rosenfeldt@GoogleMail.com>
2014-08-24 09:10:10 +0200

    JUnit4 added. Half way through  Tile.union()

---
+++ src/TileSymmetryCheck.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tile.Tile;
+
+/* Copyright 2014   Arne Christian  Rosenfeldt
+
+This file is part of Tile'n'Tree.
+
+Tile'n'Tree is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Tile'n'Tree is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Tile'n'Tree.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @author Arne Rosenfeldt
+ *
+ */
+public class TileSymmetryCheck {
+
+	@Test
+	public void test() {
+		Tile a=new Tile(2,0,0);
+		Tile b=new Tile(2,0,0);
+		if (!a.equals(b)){
+			fail("a!=b");
+		}
+
+		if (!b.equals(a)){
+			fail("b!=a");
+		}
+	}
+	
+	@Test
+	public void union() {
+		Tile a=new Tile(2,0,0);
+		Tile b=new Tile(0,0,0);
+		a.uniteWith(b);
+		if (!a.equals(b)){
+			fail("union a!=b");
+		}
+
+		if (!b.equals(a)){
+			fail("union a!=b");
+		}
+	}
+	
+
+}
--- src/tile/Tiles.java
+++ src/tile/Tiles.java
@@ -186,5 +186,19 @@ public class Tiles {
 		} while (vi.contentsLost());
 	}
 	
+	// Returns a minimal bitmap (3x3) of the character. Needed for union
+	// Adresses are bottom-up  right-left  for BigEndian  compatibility
+	// It is copy and paste from this.updateGivenTile()
+	public static int getMap(int i){
+		int r=0;
+		int m=4; // skip shading.  Todo separete shading from shape also in this class
+		if ((i & m )!=0) r|=7<<3;//g.drawLine(0, s[1]/2, s[0], s[1]/2);
+		m<<=1;
+		if ((i & m )==0) r|=4<<6;//g.drawArc(-s[0]/2, -s[1]/2, s[0], s[1], -90,90);
+		m<<=1;
+		if ((i & m )!=0) r|=4;//g.drawArc(-s[0]/2, +s[1]/2, s[0], s[1], +90,360); // ToDo explain 10 ->
+		
+		return r;
+	}	
 	
 }
