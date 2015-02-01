1cf0652a1bd6cf2d466a74fefac267284c0767c8
Arne Christian  Rosenfeldt <Arne.Rosenfeldt@GoogleMail.com>
2015-02-01 12:34:32 +0100

    post -> static -> get

--- TileAndTreeDiscrete/src/com/arnecrosenfeldt/tileandtree/TileServlet.java
+++ TileAndTreeDiscrete/src/com/arnecrosenfeldt/tileandtree/TileServlet.java
@@ -3,6 +3,7 @@ package com.arnecrosenfeldt.tileandtree;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -32,7 +33,15 @@ import com.google.appengine.api.users.UserServiceFactory;
 @SuppressWarnings("serial")
 public class TileServlet extends HttpServlet {
 
+	private static java.util.List<com.arnecrosenfeldt.tileandtree.User>  logins=new java.util.ArrayList<com.arnecrosenfeldt.tileandtree.User>(); //[3]; // in contrast with google user
+	// com.arnecrosenfeldt.tileandtree.User
 	
+	public TileServlet(){
+		// ToDo: Take cara that these static code does not end up in production.
+		logins.add(new com.arnecrosenfeldt.tileandtree.User("Hans",5,"WurstZabc",0));
+		logins.add(new com.arnecrosenfeldt.tileandtree.User("Joe Doe",1,"1234",1));
+		logins.add(new com.arnecrosenfeldt.tileandtree.User("James Bond",7,"SPECTRE",2));
+	}
 	
 	// http://stackoverflow.com/questions/15056686/sending-a-blob-to-a-servlet-through-ajax
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -52,76 +61,79 @@ public class TileServlet extends HttpServlet {
 			out.println(bytesRead);
 		}
 		*/
-		
-		
-		ServletOutputStream out=response.getOutputStream();
+
+		ServletOutputStream out = response.getOutputStream();
 		//out.write(buf); stringlängen wie in Pascal / Java intern
+
 		
-		
-		byte[] a=new byte[4]; // I need the number of bytes for each string.
 
 		// All this ugly manual memory allocation  is here to avoid that even more ugly   magic string of formData
-		
-		String NAME="Joe Doe";		
-		a[0]=(byte) NAME.length();
-		a[1]=7; // Id
-		String PASSWORD="1234";
-		a[2]=(byte) PASSWORD.length();		
-		a[3]=1; // FUNCTION_value  0=none (ident) per best practice for enums
-		out.write(a);
 
-		a=NAME.getBytes("UTF-8"); // should be optimized away as there is nothing to convert
-		out.write(a);
+		//for (int i=0;i<3;i++) {
+		for(com.arnecrosenfeldt.tileandtree.User login : logins){
+			byte[] a = new byte[4]; // I need the number of bytes for each string.
+			
+			String NAME = login.getName(); //"Joe Doe";
+			a[0] = (byte) NAME.length();
+			a[1] = (byte) login.getId();//7; // Id
+			String PASSWORD =login.getPassword() ;//"1234";
+			a[2] = (byte) PASSWORD.length();
+			a[3] = (byte) login.getFunction();//1; // FUNCTION_value  0=none (ident) per best practice for enums
+			out.write(a);
 
-		a=PASSWORD.getBytes("UTF-8"); // should be optimized away as there is nothing to convert
-		out.write(a);
-		
+			a = NAME.getBytes("UTF-8"); // should be optimized away as there is nothing to convert
+			out.write(a);
+
+			a = PASSWORD.getBytes("UTF-8"); // should be optimized away as there is nothing to convert
+			out.write(a);
+
+		}
+
 		out.close();
-		
+
 		// input.close();
 	}
 
 	// Format: count, int, int, int.  Java is big endian. JavaScript is Typed Arrays are little-endian !!??
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		InputStream input = request.getInputStream(); // ECMA script is not able to build XML. It can produce FormData where I have to provide magic markers (aaarg).
-		
-		byte[] a=new byte[4]; // I need the number of bytes for each string.
-		int count_read_elements=input.read(a);
-				
+
+		byte[] a = new byte[4]; // I need the number of bytes for each string.
+		int count_read_elements = input.read(a);
+
 		PrintWriter out = response.getWriter();
 
-		out.println("count_read_elements: "+count_read_elements);
-		
+		out.println("count_read_elements: " + count_read_elements);
+
 		// All this ugly manual memory allocation  is here to avoid that even more ugly   magic string of formData
-		int NAME_lenght=a[0];
-		out.println("NAme.length: "+NAME_lenght);
-		int ID=a[1];
-		out.println("ID: "+ID);
-		int PASSWORD_length=a[2];
-		out.println("PASSWORD_length: "+PASSWORD_length);
-		int FUNCTION_value=a[3];
-		out.println("FUNCTION_value: "+FUNCTION_value);
-		
-		 a=new byte[NAME_lenght]; // I need the number of bytes for each string.
-		 count_read_elements=input.read(a);
-			
-		String NAME=new String(a,"UTF-8");
-		
-		out.println("NAme: "+NAME);
+		int NAME_lenght = a[0];
+		out.println("NAme.length: " + NAME_lenght);
+		int ID = a[1];
+		out.println("ID: " + ID);
+		int PASSWORD_length = a[2];
+		out.println("PASSWORD_length: " + PASSWORD_length);
+		int FUNCTION_value = a[3];
+		out.println("FUNCTION_value: " + FUNCTION_value);
 
-		 a=new byte[PASSWORD_length]; // I need the number of bytes for each string.
-		 count_read_elements=input.read(a);
-			
-		String PASSWORD=new String(a,"UTF-8");
+		a = new byte[NAME_lenght]; // I need the number of bytes for each string.
+		count_read_elements = input.read(a);
 
-		out.println("PASSWORD: "+PASSWORD);
-		
-		
+		String NAME = new String(a, "UTF-8");
 
-		
+		out.println("NAme: " + NAME);
+
+		a = new byte[PASSWORD_length]; // I need the number of bytes for each string.
+		count_read_elements = input.read(a);
+
+		String PASSWORD = new String(a, "UTF-8");
+
+		out.println("PASSWORD: " + PASSWORD);
+
 		//while ((bytesRead = input.read(buffer)) != -1) {
-		
+
 		out.close();
 		input.close();
+		
+		logins.add(new com.arnecrosenfeldt.tileandtree.User(NAME,ID,PASSWORD,FUNCTION_value));
 	}
 }
